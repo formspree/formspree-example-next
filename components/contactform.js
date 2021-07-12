03454ebe9e623854190425d859a34092c592298e
Cole <cole@colevscode.io>
2021-07-11 22:54:31 -0700

    updating form styles

--- components/contactform.js
+++ components/contactform.js
@@ -1,30 +1,6 @@
 import * as React from "react";
 import { FormspreeProvider, useForm, ValidationError } from "@formspree/react";
-
-// Some nice boilerplate CSS to tidy up your form
-const form = {
-  display: "grid",
-  gridTemplateColumns: "max-content 1fr",
-  gridGap: "1rem",
-  textAlign: "left",
-  padding: "2rem 0"
-};
-const formInputs = {
-  fontFamily: "inherit",
-  fontSize: "inherit",
-  border: "1px solid currentColor",
-  background: "none",
-  padding: "0.4rem 0.5rem",
-  margin: 0
-};
-const formTextarea = {
-  ...formInputs,
-  resize: "vertical"
-};
-const formButton = {
-  ...formInputs,
-  justifySelf: "start"
-};
+import formStyles from "../styles/form.module.css";
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("contact");
@@ -32,14 +8,18 @@ function ContactForm() {
     return <p>Thanks for your submission!</p>;
   }
   return (
-    <form onSubmit={handleSubmit} style={form}>
+    <form onSubmit={handleSubmit} className={formStyles.container}>
       <label htmlFor="email">Your email:</label>
-      <input id="email" type="email" name="email" style={formInputs} />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <input id="email" type="email" name="email" className={formStyles.inputs} />
+      <ValidationError prefix="Email" field="email" errors={state.errors} className={formStyles.errors} />
       <label htmlFor="message">Your message:</label>
-      <textarea id="message" name="message" style={formTextarea} />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button type="submit" disabled={state.submitting} style={formButton}>
+      <textarea 
+        id="message" 
+        name="message"
+        className={`${formStyles.inputs} ${formStyles.textarea}`}
+      />
+      <ValidationError prefix="Message" field="message" errors={state.errors} className={formStyles.errors} />
+      <button type="submit" disabled={state.submitting} className={`${formStyles.inputs} ${formStyles.button}`}>
         Send
       </button>
     </form>
